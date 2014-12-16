a431941e8448db856d47ac5fe8e8422c130ae979
ronald (MacBookie Pro) <ronald@orangewise.com>
2014-12-16 11:30:00 +0100

    first commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'robodo:meteor-debug',
+  summary: 'Meteor wrapper for npm debug.',
+  version: '2.1.0'
+});
+
+Npm.depends({'debug': '2.1.0'});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles('debug.js','client');
+  api.addFiles('server.js','server');
+  api.export('debugs');
+});
