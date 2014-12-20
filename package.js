0d44a30666de5cdd4c45b97e6b84671ee721f0a9
ronald (MacBookie Pro) <ronald@orangewise.com>
2014-12-20 12:22:26 +0100

    removed single and double quotes mix

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'robodo:meteor-debug',
   summary: 'Meteor wrapper for npm debug',
   version: '0.0.3',
-  git: "https://github.com/orangewise/meteor-debug"
+  git: 'https://github.com/orangewise/meteor-debug'
 });
 
 Npm.depends({'debug': '2.1.0'});
